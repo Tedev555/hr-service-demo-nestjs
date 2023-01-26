749487722e0a546c95159786310b3476a4d555bc
Ted-AIF <thanongsine@aifgrouplaos.com>
2023-01-26 17:04:22 +0700

    Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { ExampleController } from './controllers/example.controller';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CatsModule],
+  controllers: [AppController, ExampleController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ test/cats.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CatsController } from '../src/cats/cats.controller';
+import { CatsService } from '../src/services/cats.service';
+
+describe('CatsController', () => {
+  let controller: CatsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CatsController],
+      providers: [CatsService],
+    }).compile();
+
+    controller = module.get<CatsController>(CatsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should return "Hello World!"', () => {
+    expect(controller.getHello()).toBe('Hello World!');
+  });
+});
