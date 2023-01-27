30d1b2f63e19985eb69e47cc22b3e4c3dace1721
Ted555 <thanong984@gmail.com>
2023-01-27 22:07:39 +0700

    Imlement basic UserService

--- __tests__/services/user.service.spec.ts
+++ __tests__/services/user.service.spec.ts
@@ -2,25 +2,72 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../../src/services/user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepositoryMock: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+    // const module: TestingModule = await Test.createTestingModule({
+    //   providers: [UserService],
+    // }).compile();
 
-    service = module.get<UserService>(UserService);
+    // service = module.get<UserService>(UserService);
+    userRepositoryMock = {
+      findAll: jest.fn(),
+      find: jest.fn(),
+      findById: jest.fn(),
+      create: jest.fn(),
+    };
+    userService = new UserService(userRepositoryMock);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 
-  // Test case for the method findAll()
-  it('should return an array of users', async () => {
-    const result = ['test'];
-    jest.spyOn(service, 'findAll').mockImplementation(() => result);
+  it('userService.create should accept a user as a parameter', async () => {
+    const user = {
+      id: 1,
+      name: 'John Doe',
+    };
 
-    expect(await service.findAll()).toBe(result);
+    await userService.create(user);
+    expect(userRepositoryMock.create).toBeCalledWith(user);
+  });
+
+  it('called userRepository.create(user) should return a user', async () => {
+    const user = {
+      id: 1,
+      name: 'John Doe',
+    };
+
+    userRepositoryMock.create.mockReturnValue(user);
+    expect(await userService.create(user)).toStrictEqual(user);
+  });
+
+  it('should return an array of users', async () => {
+    const result = [
+      {
+        id: 1,
+        name: 'John Doe',
+      },
+    ];
+    userRepositoryMock.findAll.mockReturnValue(result);
+    expect(await userService.findAllUsers()).toStrictEqual(result);
+  });
+
+  it('userRepository.findById() should be called with id', async () => {
+    const id = 1;
+    await userService.findById(id);
+    expect(userRepositoryMock.findById).toBeCalledWith(id);
+  });
+
+  it('called userRepository.findById() should return a user', async () => {
+    const result = {
+      id: 1,
+      name: 'John Doe',
+    };
+
+    userRepositoryMock.findById.mockReturnValue(result);
+    expect(await userService.findById(result.id)).toStrictEqual(result);
   });
 });
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private userRepository: any) {}
+
+  findAllUsers(): any[] {
+    return this.userRepository.findAll();
+  }
+
+  findById(userId: number) {
+    return this.userRepository.findById(userId);
+  }
+
+  create(user: any) {
+    return this.userRepository.create(user);
+  }
+}
