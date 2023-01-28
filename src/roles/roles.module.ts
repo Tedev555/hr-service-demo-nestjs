83e7ae1cdb2f7462509ce9b53208faf97d7a91ce
Ted555 <thanong984@gmail.com>
2023-01-28 08:46:34 +0700

    setup roles

---
+++ src/roles/roles.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { RolesController } from './roles.controller';
+import { RolesRepository } from './roles.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [RolesController],
+  providers: [RolesService, RolesRepository],
+})
+export class RolesModule {}
